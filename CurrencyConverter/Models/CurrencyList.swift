6c0daaaf737f4fa3c3606271a7fd49035dc3cf85
ms2138 <m38371@gmail.com>
2020-08-02 20:54:28 -0700

    Add Codable protocol conformance.  Add decode and encode methods to parse json response with the results key in CurrencyList

--- CurrencyConverter/Models/CurrencyList.swift
+++ CurrencyConverter/Models/CurrencyList.swift
@@ -8,9 +8,10 @@
 
 import Foundation
 
-struct CurrencyList {
+struct CurrencyList: Codable {
     var currencies: [Currency]
 
+    // Add coding key to handle the root results key in the json response
     struct CurrencyKey : CodingKey {
         var stringValue: String
         init?(stringValue: String) {
@@ -23,5 +24,35 @@ struct CurrencyList {
         static let name = CurrencyKey(stringValue: "currencyName")!
         static let symbol = CurrencyKey(stringValue: "currencySymbol")!
         static let id = CurrencyKey(stringValue: "id")!
+    }
+
+    // Add custom decoding and encoding to parse response with root results key
+    public init(from decoder: Decoder) throws {
+        var currencies = [Currency]()
+        let container = try decoder.container(keyedBy: CurrencyKey.self)
+        let resultsContainer = try container.nestedContainer(keyedBy: CurrencyKey.self, forKey: .results)
+        for key in resultsContainer.allKeys {
+            let nestedContainer = try resultsContainer.nestedContainer(keyedBy: CurrencyKey.self, forKey: key)
+            let name = try nestedContainer.decode(String.self, forKey: .name)
+            let symbol = try nestedContainer.decodeIfPresent(String.self, forKey: .symbol)
+            let id = try nestedContainer.decode(String.self, forKey: .id)
+
+            let currency = Currency(name: name, symbol: symbol, id: id)
+            currencies.append(currency)
+        }
+        self.currencies = currencies
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CurrencyKey.self)
+
+        for currency in currencies {
+            let nameKey = CurrencyKey(stringValue: currency.name)!
+            var currencyContainer = container.nestedContainer(keyedBy: CurrencyKey.self, forKey: nameKey)
+
+            try currencyContainer.encode(currency.name, forKey: .name)
+            try currencyContainer.encode(currency.symbol, forKey: .symbol)
+            try currencyContainer.encode(currency.id, forKey: .id)
+        }
     }
 }
