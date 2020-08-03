25d7a1062e271586863aa4c71c7d1776cae6346d
ms2138 <m38371@gmail.com>
2020-08-03 00:11:07 -0700

    Add RateKey struct.  Add Codable protocol conformance to ExchangeRate.  Add init method to decode exchange rate from json response

--- CurrencyConverter/Models/ExchangeRate.swift
+++ CurrencyConverter/Models/ExchangeRate.swift
@@ -8,6 +8,24 @@
 
 import Foundation
 
-struct ExchangeRate {
+struct ExchangeRate: Codable {
     var rate: Double
+
+    struct RateKey : CodingKey {
+        var stringValue: String
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+        var intValue: Int? { return nil }
+        init?(intValue: Int) { return nil }
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: RateKey.self)
+        var exchange = Double(0.0)
+        for key in container.allKeys {
+            exchange = try container.decode(Double.self, forKey: key)
+        }
+        self.rate = exchange
+    }
 }
