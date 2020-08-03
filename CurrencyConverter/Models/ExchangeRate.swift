0a4cbdec593f672e894d34fa6ecc01551c3cb57e
ms2138 <m38371@gmail.com>
2020-08-02 20:55:23 -0700

    Add ExchangeRate struct

---
+++ CurrencyConverter/Models/ExchangeRate.swift
@@ -0,0 +1,12 @@
+//
+//  ExchangeRate.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-08-02.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+struct ExchangeRate {
+}
