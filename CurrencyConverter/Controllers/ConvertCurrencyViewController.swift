7be496c4ca0fecc38091a334cb3f3b7e67fc5343
ms2138 <m38371@gmail.com>
2020-08-02 20:02:31 -0700

    Remove ViewController.  Add ConvertCurrencyViewController class and Controllers folder.  Move ConvertCurrencyViewController to Controllers folder.  Change Main.storyboard view controller to ConvertCurrencyViewController

--- CurrencyConverter/Controllers/ConvertCurrencyViewController.swift
+++ CurrencyConverter/Controllers/ConvertCurrencyViewController.swift
@@ -1,20 +1,16 @@
 //
-//  ViewController.swift
+//  ConvertCurrencyViewController.swift
 //  CurrencyConverter
 //
-//  Created by admin on 2020-08-02.
+//  Created by mani on 2020-08-02.
 //  Copyright © 2020 mani. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConvertCurrencyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
