7a813068e91dd656cfcc5de32e189b8230197859
ms2138 <m38371@gmail.com>
2020-08-03 22:52:29 -0700

    Add apiKey property and createURL method to CurrencyDataDownloader

--- CurrencyConverter/Data Handlers/CurrencyDataDownloader.swift
+++ CurrencyConverter/Data Handlers/CurrencyDataDownloader.swift
@@ -9,4 +9,15 @@
 import Foundation
 
 class CurrencyDataDownloader {
+    private let apiKey = "ENTER KEY HERE"
+
+    private func createURL(path: String, queryItems: [URLQueryItem] = []) -> URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "free.currencyconverterapi.com"
+        components.path = "/api/v7/\(path)"
+        components.queryItems = queryItems
+        components.queryItems?.append(URLQueryItem(name: "apiKey", value: apiKey))
+        return components.url
+    }
 }
