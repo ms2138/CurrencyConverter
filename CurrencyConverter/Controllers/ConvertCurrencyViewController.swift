2946ed7e198f6205ef8986020dbdfe93431830e7
ms2138 <m38371@gmail.com>
2020-08-03 22:28:26 -0700

    Add convertButton IBOutlet property.  Connect ConvertCurrencyViewController's convertButton in Main.storyboard

--- CurrencyConverter/Controllers/ConvertCurrencyViewController.swift
+++ CurrencyConverter/Controllers/ConvertCurrencyViewController.swift
@@ -14,6 +14,7 @@ class ConvertCurrencyViewController: UIViewController {
     @IBOutlet weak var convertFromButton: UIButton!
     @IBOutlet weak var convertToButton: UIButton!
     @IBOutlet weak var switchConversionButton: UIButton!
+    @IBOutlet weak var convertButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
