7566f8a2135274f29e743e3f6925b99d9316ce65
ms2138 <m38371@gmail.com>
2020-08-03 21:39:03 -0700

    Add CustomButton class.  Add Custom Views folder.  Move CustomButton in to Custom Views folder

---
+++ CurrencyConverter/Custom Views/CustomButton.swift
@@ -0,0 +1,13 @@
+//
+//  CustomButton.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-08-03.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomButton: UIButton {
+}
