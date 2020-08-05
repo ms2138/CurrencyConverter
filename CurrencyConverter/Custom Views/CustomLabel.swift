02f47bb27bf60d3fb19c1dfd48218d5a656066f4
ms2138 <m38371@gmail.com>
2020-08-04 21:34:40 -0700

    Add override for canPerformAction and copy methods to CustomLabel

--- CurrencyConverter/Custom Views/CustomLabel.swift
+++ CurrencyConverter/Custom Views/CustomLabel.swift
@@ -12,4 +12,12 @@ class CustomLabel: UILabel {
     override var canBecomeFirstResponder: Bool {
         return true
     }
+
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return (action == #selector(UIResponderStandardEditActions.copy(_:)))
+    }
+
+    override func copy(_ sender: Any?) {
+        UIPasteboard.general.string = text
+    }
 }
