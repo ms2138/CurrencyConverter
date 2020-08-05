2755c3f5a622a25fb4af0c2c5df89f59de5b2615
ms2138 <m38371@gmail.com>
2020-08-04 22:01:35 -0700

    Add CurrencyDataManager class

---
+++ CurrencyConverter/Data Managers/CurrencyDataManager.swift
@@ -0,0 +1,12 @@
+//
+//  CurrencyDataManager.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-08-04.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+class CurrencyDataManager {
+}
