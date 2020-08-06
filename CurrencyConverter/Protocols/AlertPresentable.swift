a0d472cccf165b3c56e27349f915c97b88c7967c
ms2138 <m38371@gmail.com>
2020-08-05 23:02:04 -0700

    Add presentAlert method to AlertPresentable.  Add AlertPresentable extension to provide default implementation for presentAlert method

--- CurrencyConverter/Protocols/AlertPresentable.swift
+++ CurrencyConverter/Protocols/AlertPresentable.swift
@@ -6,7 +6,17 @@
 //  Copyright © 2020 mani. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 protocol AlertPresentable where Self: UIViewController {
+    func presentAlert(title: String, message: String, handler: ((UIAlertAction) -> Void)?)
+}
+
+extension AlertPresentable {
+    func presentAlert(title: String, message: String, handler: ((UIAlertAction) -> Void)? = nil) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: handler)
+        alertController.addAction(action)
+        self.present(alertController, animated: true)
+    }
 }
