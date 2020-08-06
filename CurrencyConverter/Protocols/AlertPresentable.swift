c8a62124b703781b56acbe098398c486c93ce25d
ms2138 <m38371@gmail.com>
2020-08-05 22:56:50 -0700

    Add AlertPresentable protocol.  Add Protocols folder.  Move AlertPresentable to Protocols folder

---
+++ CurrencyConverter/Protocols/AlertPresentable.swift
@@ -0,0 +1,12 @@
+//
+//  AlertPresentable.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-08-05.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+protocol AlertPresentable where Self: UIViewController {
+}
