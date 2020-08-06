e229d1d457e2d22dd02a6031a76365e2c7fb8b37
ms2138 <m38371@gmail.com>
2020-08-05 22:21:22 -0700

    Add Constants struct

---
+++ CurrencyConverter/Helpers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-08-05.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+}
