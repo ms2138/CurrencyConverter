30bc95fc3995d6ee846bd2a6a5047cfc5916fd8d
ms2138 <m38371@gmail.com>
2020-08-06 22:31:50 -0700

    Add CurrencySelectorViewController class

---
+++ CurrencyConverter/Controllers/CurrencySelectorViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CurrencySelectorViewController.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-08-06.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class CurrencySelectorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
