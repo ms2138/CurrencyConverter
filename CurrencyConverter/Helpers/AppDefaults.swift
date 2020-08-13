b9091cb2e39cb5083a2446d164eab93f1598e467
ms2138 <m38371@gmail.com>
2020-08-12 21:00:59 -0700

    Add selectedCurrencyConversion property to AppDefaults

--- CurrencyConverter/Helpers/AppDefaults.swift
+++ CurrencyConverter/Helpers/AppDefaults.swift
@@ -28,5 +28,26 @@ class AppDefaults {
             defaults.set(newValue, forKey: Constants.cacheTimestamp)
         }
     }
+    var selectedCurrencyConversion: CurrencyConversion {
+        get {
+            if let savedCurrencyConversion = defaults.object(forKey: Constants.selectedCurrencyConversion) as? Data {
+                let decoder = JSONDecoder()
+                if let decodedCurrencyConversion = try? decoder.decode(CurrencyConversion.self, from: savedCurrencyConversion) {
+                    return decodedCurrencyConversion
+                }
+            }
+            // Provide a default currency conversion
+            let fromCurrency = Currency.init(name: "United States Dollar", symbol: "$", id: "USD")
+            let toCurrency = Currency.init(name: "Canadian Dollar", symbol: "$", id: "CAD")
+            return CurrencyConversion.init(from: fromCurrency, to: toCurrency)
+        }
+        set {
+            let encoder = JSONEncoder()
+            if let encodedCurrencyConversion = try? encoder.encode(newValue) {
+                defaults.set(encodedCurrencyConversion, forKey: Constants.selectedCurrencyConversion)
+            }
+        }
+
+    }
 }
 
