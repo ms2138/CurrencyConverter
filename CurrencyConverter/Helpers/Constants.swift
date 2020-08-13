b20fe3c99e806cee71fb6f87af11410ae27d40cd
ms2138 <m38371@gmail.com>
2020-08-12 20:37:14 -0700

    Add selectedCurrencyConversion property to Constants

--- CurrencyConverter/Helpers/Constants.swift
+++ CurrencyConverter/Helpers/Constants.swift
@@ -11,4 +11,5 @@ import Foundation
 struct Constants {
     static let exchangeRateCache = "ExchangeRateCache"
     static let cacheTimestamp = "CacheTimestamp"
+    static let selectedCurrencyConversion = "SelectedCurrencyConversion"
 }
