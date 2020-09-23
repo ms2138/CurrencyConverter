2c4a0ac1506fd7760f143b02867c635621dc1f78
ms2138 <m38371@gmail.com>
2020-09-22 20:51:30 -0700

    Add CurrencyDataController extension to provide a default implementation of the loadCurrencies method

--- CurrencyConverter/Protocols/CurrencyDataController.swift
+++ CurrencyConverter/Protocols/CurrencyDataController.swift
@@ -13,3 +13,26 @@ protocol CurrencyDataController where Self: UIViewController  {
 
     func loadCurrencies(completion: (() -> Void)?)
 }
+
+extension CurrencyDataController {
+    func loadCurrencies(completion: (() -> Void)? = nil) {
+        if let url = CurrencyURL().currencyURL {
+            let currencyDownloader = CurrencyDataRequest(url: url)
+            currencyDownloader.getData { [weak self] result in
+                guard let weakSelf = self else { return }
+                switch result {
+                    case .success(let data):
+                        do {
+                            weakSelf.currencies = try CurrencyDataDecoder(data: data).decode(type: CurrencyList.self).currencies
+                            weakSelf.currencies.sort { $0.name < $1.name }
+                        } catch {
+                            debugLog("Failed to get currencies")
+                    }
+                    case .failure(let error):
+                        debugLog("Error: \(error.localizedDescription)")
+                }
+                completion?()
+            }
+        }
+    }
+}
