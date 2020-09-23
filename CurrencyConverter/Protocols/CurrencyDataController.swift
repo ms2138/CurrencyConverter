c09f40675166e717651608a37616ab345dd0e4cb
ms2138 <m38371@gmail.com>
2020-09-22 20:43:31 -0700

    CurrencyDataController protocol

---
+++ CurrencyConverter/Protocols/CurrencyDataController.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyDataController.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-09-22.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+protocol CurrencyDataController where Self: UIViewController  {
+    
+}
