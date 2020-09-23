7ae86576af30d23df032aad229e67e3ceda70200
ms2138 <m38371@gmail.com>
2020-09-22 20:25:16 -0700

    Add currencyURL property and getExchangeRateURL method to CurrencyURL

--- CurrencyConverter/CurrencyURL.swift
+++ CurrencyConverter/CurrencyURL.swift
@@ -11,6 +11,9 @@ import Foundation
 class CurrencyURL {
     #warning("Incomplete implementation.  Please register at https://free.currencyconverterapi.com/free-api-key to get an api key")
     private let apiKey = "ENTER KEY HERE"
+    var currencyURL: URL? {
+        return createBaseURL(path: "currencies")
+    }
 
     private func createBaseURL(path: String, queryItems: [URLQueryItem] = []) -> URL? {
         var components = URLComponents()
@@ -20,5 +23,13 @@ class CurrencyURL {
         components.queryItems = queryItems
         components.queryItems?.append(URLQueryItem(name: "apiKey", value: apiKey))
         return components.url
+    }
+}
+
+extension CurrencyURL {
+    func getExchangeRateURL(from conversion: CurrencyConversion) -> URL? {
+        let queryItems = [URLQueryItem(name: "q", value: "\(conversion.from.id)_\(conversion.to.id)"),
+                          URLQueryItem(name: "compact", value: "ultra")]
+        return createBaseURL(path: "convert", queryItems: queryItems)
     }
 }
