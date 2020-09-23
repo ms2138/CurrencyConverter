0e1699a24c46a4b22cd4dbbd16d14713143c30d0
ms2138 <m38371@gmail.com>
2020-09-22 19:40:36 -0700

    Add CurrencyDataRequest class

---
+++ CurrencyConverter/Data Handlers/CurrencyDataRequest.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyDataRequest.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-09-22.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+class CurrencyDataRequest {
+
+}
