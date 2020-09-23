f16dbb19640d474ab2435831d9937654a32e18c4
ms2138 <m38371@gmail.com>
2020-09-22 20:49:35 -0700

    Add data property and init and decode methods to CurrencyDataDecoder

--- CurrencyConverter/Data Handlers/CurrencyDataDecoder.swift
+++ CurrencyConverter/Data Handlers/CurrencyDataDecoder.swift
@@ -9,5 +9,14 @@
 import Foundation
 
 class CurrencyDataDecoder {
+    let data: Data
 
+    init(data: Data) {
+        self.data = data
+    }
+
+    func decode<T>(type: T.Type) throws -> T where T : Decodable {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    }
 }
