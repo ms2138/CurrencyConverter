5aa130b34a2bc9cf36680857c480eb7ded003fd4
ms2138 <m38371@gmail.com>
2020-09-22 20:48:17 -0700

    Add CurrencyDataDecoder class

---
+++ CurrencyConverter/Data Handlers/CurrencyDataDecoder.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyDataDecoder.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-09-22.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+class CurrencyDataDecoder {
+
+}
