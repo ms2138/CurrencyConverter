41d5502feba64181d834963c9d87cbefb724f36d
ms2138 <m38371@gmail.com>
2020-09-23 20:40:57 -0700

    Add CurrencyStorageManagerTests class

---
+++ CurrencyConverterTests/CurrencyStorageManagerTests.swift
@@ -0,0 +1,22 @@
+//
+//  CurrencyStorageManagerTests.swift
+//  CurrencyConverterTests
+//
+//  Created by mani on 2020-09-23.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import XCTest
+
+@testable import CurrencyConverter
+
+class CurrencyStorageManagerTests: XCTestCase {
+
+    override func setUp() {
+
+    }
+
+    override func tearDown() {
+
+    }
+}
