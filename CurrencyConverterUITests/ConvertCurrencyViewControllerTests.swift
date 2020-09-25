5e4f35c57dac20e760a1c7e69c3baf7d0510c41a
ms2138 <m38371@gmail.com>
2020-09-24 21:41:07 -0700

    Add testIntialOutputDisplayLabelText and testNumberPad methods to ConvertCurrencyViewControllerTests

--- CurrencyConverterUITests/ConvertCurrencyViewControllerTests.swift
+++ CurrencyConverterUITests/ConvertCurrencyViewControllerTests.swift
@@ -9,8 +9,11 @@
 import XCTest
 
 class ConvertCurrencyViewControllerTests: XCTestCase {
+    var helper: ConvertCurrencyViewControllerHelper!
 
     override func setUp() {
+        helper = ConvertCurrencyViewControllerHelper()
+
         continueAfterFailure = false
 
         XCUIApplication().launch()
@@ -19,4 +22,30 @@ class ConvertCurrencyViewControllerTests: XCTestCase {
     override func tearDown() {
     }
 
+    func testIntialOutputDisplayLabelText() {
+        let displayLabel = helper.outputDisplayLabel
+        XCTAssertTrue(displayLabel.exists)
+        XCTAssertEqual(displayLabel.label, "0")
+    }
+
+    func testNumberPad() {
+        let app = XCUIApplication()
+
+        let displayLabel = helper.outputDisplayLabel
+
+        var display = ""
+        for i in (0...9).reversed() {
+            let numberButton = app.buttons.containing(.staticText, identifier:"\(i)").element
+            numberButton.tap()
+            display += "\(i)"
+            XCTAssertTrue(numberButton.exists)
+            XCTAssertEqual(displayLabel.label, "\(display)")
+        }
+
+        let decimalButton = app.buttons.containing(.staticText, identifier:".").element
+        decimalButton.tap()
+        display += "."
+        XCTAssertTrue(decimalButton.exists)
+        XCTAssertEqual(displayLabel.label, "\(display)")
+    }
 }
