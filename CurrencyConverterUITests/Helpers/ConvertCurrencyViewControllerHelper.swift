721457946b7a8fc57e699b29141c5fdb31374099
ms2138 <m38371@gmail.com>
2020-09-24 20:35:39 -0700

    Add ConvertCurrencyViewControllerHelper class.  Add Helpers folder.  Move ConvertCurrencyViewControllerHelper to Helpers folder

---
+++ CurrencyConverterUITests/Helpers/ConvertCurrencyViewControllerHelper.swift
@@ -0,0 +1,13 @@
+//
+//  ConvertCurrencyViewControllerHelper.swift
+//  CurrencyConverterUITests
+//
+//  Created by mani on 2020-09-24.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import XCTest
+
+class ConvertCurrencyViewControllerHelper {
+
+}
