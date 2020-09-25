4600712a9b35e9db1813f658d70a1e3e171a798f
ms2138 <m38371@gmail.com>
2020-09-24 19:14:33 -0700

    Add currencies.json and exchangeRate.json files to provide local currency data.  Add CurrencyDataDecoderTests class

---
+++ CurrencyConverterTests/CurrencyDataDecoderTests.swift
@@ -0,0 +1,22 @@
+//
+//  CurrencyDataDecoderTests.swift
+//  CurrencyConverterTests
+//
+//  Created by mani on 2020-09-23.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import XCTest
+
+@testable import CurrencyConverter
+
+class CurrencyDataDecoderTests: XCTestCase {
+
+    override func setUp() {
+
+    }
+
+    override func tearDown() {
+
+    }
+}
