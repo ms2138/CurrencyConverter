cc5d63800a791d2f47176ee5643eda8adb4749cf
ms2138 <m38371@gmail.com>
2020-09-24 20:37:11 -0700

    Add AccessibilityIdentifier enum

---
+++ CurrencyConverter/AccessibilityIdentifier.swift
@@ -0,0 +1,12 @@
+//
+//  AccessibilityIdentifier.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-09-24.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+enum AccessibilityIdentifier: String {
+}
