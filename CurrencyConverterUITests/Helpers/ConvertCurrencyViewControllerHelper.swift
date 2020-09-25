d5171ac8842c9d5573ff62b7fac8c6d2491836cd
ms2138 <m38371@gmail.com>
2020-09-24 20:50:27 -0700

    Add outputDisplayLabel, exchangeRateDisplayLabel, convertFromButton, and convertToButton properties to  ConvertCurrencyViewControllerHelper

--- CurrencyConverterUITests/Helpers/ConvertCurrencyViewControllerHelper.swift
+++ CurrencyConverterUITests/Helpers/ConvertCurrencyViewControllerHelper.swift
@@ -9,5 +9,19 @@
 import XCTest
 
 class ConvertCurrencyViewControllerHelper {
+    var outputDisplayLabel: XCUIElement {
+        return XCUIApplication().buttons["OutputDisplayLabel"]
+    }
 
+    var exchangeRateDisplayLabel: XCUIElement {
+        return XCUIApplication().buttons["ExchangeRateDisplayLabel"]
+    }
+
+    var convertFromButton: XCUIElement {
+        return XCUIApplication().buttons["ConvertFromButton"]
+    }
+
+    var convertToButton: XCUIElement {
+        return XCUIApplication().buttons["ConvertToButton"]
+    }
 }
