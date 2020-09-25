bcddab7d2caa0978bd3e21655d24e547756cf9da
ms2138 <m38371@gmail.com>
2020-09-24 20:03:22 -0700

    Add loadData helper, testCurrenciesDecodingSuccess and testExchangeRateDecodingSuccess methods to CurrencyDataDecoderTests

--- CurrencyConverterTests/CurrencyDataDecoderTests.swift
+++ CurrencyConverterTests/CurrencyDataDecoderTests.swift
@@ -19,4 +19,31 @@ class CurrencyDataDecoderTests: XCTestCase {
     override func tearDown() {
 
     }
+
+    func testCurrenciesDecodingSuccess() {
+        let data = loadData(forResource: "exchangeRate", extension: "json")
+        let currencyDataDecoder = CurrencyDataDecoder(data: data)
+
+        let currencies = try! currencyDataDecoder.decode(type: CurrencyList.self).currencies
+
+        XCTAssertNotNil(currencies)
+        XCTAssertNotEqual(currencies.count, 0)
+    }
+
+    func testExchangeRateDecodingSuccess() {
+        let data = loadData(forResource: "exchangeRate", extension: "json")
+        let currencyDataDecoder = CurrencyDataDecoder(data: data)
+
+        let exchange = try! currencyDataDecoder.decode(type: ExchangeRate.self)
+
+        XCTAssertNotNil(exchange)
+        XCTAssertEqual(exchange.rate, 1.339965)
+    }
+
+    func loadData(forResource resource: String, extension ext: String) -> Data {
+        let bundle = Bundle(for: type(of: self))
+
+        let path = bundle.url(forResource: resource, withExtension: ext)!
+        return try! Data(contentsOf: path)
+    }
 }
