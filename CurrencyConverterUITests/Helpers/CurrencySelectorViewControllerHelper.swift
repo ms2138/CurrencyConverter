dccb568a466c90ac1c1084f0226f83bbda9d2b4d
ms2138 <m38371@gmail.com>
2020-09-27 21:49:17 -0700

    Add CurrencySelectorViewControllerHelper class

---
+++ CurrencyConverterUITests/Helpers/CurrencySelectorViewControllerHelper.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencySelectorViewControllerHelper.swift
+//  CurrencyConverterUITests
+//
+//  Created by mani on 2020-09-27.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import XCTest
+
+class CurrencySelectorViewControllerHelper {
+
+}
