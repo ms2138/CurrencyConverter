1b8ce336b4dfdba3568d9aa5e3400b322a0136a2
ms2138 <m38371@gmail.com>
2020-09-27 21:40:36 -0700

    Add CurrencySelectorViewControllerTests class

---
+++ CurrencyConverterUITests/CurrencySelectorViewControllerTests.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencySelectorViewControllerTests.swift
+//  CurrencyConverterUITests
+//
+//  Created by mani on 2020-09-27.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import XCTest
+
+class CurrencySelectorViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+
+        XCUIApplication().launch()
+    }
+
+    override func tearDown() {
+    }
+}
