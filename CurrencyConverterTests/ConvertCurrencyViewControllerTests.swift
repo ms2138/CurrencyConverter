494538f31bc5e5d985f894a991686b904ed2cbf9
ms2138 <m38371@gmail.com>
2020-09-28 22:04:51 -0700

    Add unit test class ConvertCurrencyViewControllerTests

---
+++ CurrencyConverterTests/ConvertCurrencyViewControllerTests.swift
@@ -0,0 +1,19 @@
+//
+//  ConvertCurrencyViewControllerTests.swift
+//  CurrencyConverterTests
+//
+//  Created by mani on 2020-09-28.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import XCTest
+
+class ConvertCurrencyViewControllerTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+}
