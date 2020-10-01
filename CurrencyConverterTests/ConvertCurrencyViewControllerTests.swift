eb57386ae9bc77dbcdaa1078cb1046d19ea3236f
ms2138 <m38371@gmail.com>
2020-09-30 22:34:47 -0700

    Add vc property and testViewNotNil method to ConvertCurrencyViewControllerTests.  Add functionality to initialize vc property in setUp method

--- CurrencyConverterTests/ConvertCurrencyViewControllerTests.swift
+++ CurrencyConverterTests/ConvertCurrencyViewControllerTests.swift
@@ -8,12 +8,22 @@
 
 import XCTest
 
+@testable import CurrencyConverter
+
 class ConvertCurrencyViewControllerTests: XCTestCase {
+    var vc: ConvertCurrencyViewController!
 
     override func setUp() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        vc = storyboard.instantiateInitialViewController() as? ConvertCurrencyViewController
+        UIApplication.shared.windows.first { $0.isKeyWindow }?.rootViewController = vc
     }
 
     override func tearDown() {
+        vc = nil
     }
 
+    func testViewNotNil() {
+        XCTAssertNotNil(vc.view)
+    }
 }
