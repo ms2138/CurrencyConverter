14fb597b2202f29e755a7d537ea32f1ea801fd32
ms2138 <m38371@gmail.com>
2020-10-06 21:03:43 -0700

    Add CurrencyConvertDataRequestTests class

---
+++ CurrencyConverterAsyncTests/CurrencyConvertDataRequestTests.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyConvertDataRequestTests.swift
+//  CurrencyConverterAsyncTests
+//
+//  Created by mani on 2020-10-06.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import XCTest
+
+class CurrencyConvertDataRequestTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+}
