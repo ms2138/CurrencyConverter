f974af3cb9e8d86ed6c5d38513612254add00cc5
ms2138 <m38371@gmail.com>
2020-10-08 20:45:04 -0700

    Add testGetExchangeRateDataSuccess and testDataFailure methods to CurrencyConvertDataRequestTests

--- CurrencyConverterAsyncTests/CurrencyConvertDataRequestTests.swift
+++ CurrencyConverterAsyncTests/CurrencyConvertDataRequestTests.swift
@@ -36,5 +36,48 @@ class CurrencyConvertDataRequestTests: XCTestCase {
         wait(for: [promise], timeout: 4)
     }
 
-    
+    func testGetExchangeRateDataSuccess() {
+        let fromCurrency = Currency.init(name: "United States Dollar", symbol: "$", id: "USD")
+        let toCurrency = Currency.init(name: "Canadian Dollar", symbol: "$", id: "CAD")
+        let conversion = CurrencyConversion.init(from: fromCurrency, to: toCurrency)
+
+        let url = CurrencyURL().getExchangeRateURL(from: conversion)!
+
+        let promise = expectation(description: "Data downloaded")
+
+        let dataRequest = CurrencyDataRequest(url: url)
+        dataRequest.getData { result in
+            switch result {
+                case .success(let data):
+                    XCTAssertNotNil(data)
+                    promise.fulfill()
+                case .failure(let error):
+                    XCTFail("Error: \(error.localizedDescription)")
+            }
+        }
+
+        wait(for: [promise], timeout: 4)
+    }
+
+    func testDataFailure() {
+        let url = URL(string: "http://free.currencyconverterapi.com")!
+        var error: Error?
+
+        let promise = expectation(description: "Expect an error")
+
+        let dataRequest = CurrencyDataRequest(url: url)
+        dataRequest.getData { result in
+            switch result {
+                case .failure(let theError):
+                    error = theError
+                    promise.fulfill()
+                case .success(_):
+                    XCTFail("Data was returned")
+            }
+        }
+
+        wait(for: [promise], timeout: 4)
+
+        XCTAssertNotNil(error)
+    }
 }
