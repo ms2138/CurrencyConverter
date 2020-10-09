f6cd53f0ab3fc1c3e75fe53ba36ec9cbe0a7118d
ms2138 <m38371@gmail.com>
2020-10-08 20:08:56 -0700

    Add testGetCurrencyDataSuccess method to CurrencyConvertDataRequestTests.  Add import statement for CurrencyConverter

--- CurrencyConverterAsyncTests/CurrencyConvertDataRequestTests.swift
+++ CurrencyConverterAsyncTests/CurrencyConvertDataRequestTests.swift
@@ -8,11 +8,33 @@
 
 import XCTest
 
-class CurrencyConvertDataRequestTests: XCTestCase {
+@testable import CurrencyConverter
 
+class CurrencyConvertDataRequestTests: XCTestCase {
     override func setUp() {
     }
 
     override func tearDown() {
     }
+
+    func testGetCurrencyDataSuccess() {
+        let url = CurrencyURL().currencyURL!
+
+        let promise = expectation(description: "Data downloaded")
+
+        let dataRequest = CurrencyDataRequest(url: url)
+        dataRequest.getData { result in
+            switch result {
+                case .success(let data):
+                    XCTAssertNotNil(data)
+                    promise.fulfill()
+                case .failure(let error):
+                    XCTFail("Error: \(error.localizedDescription)")
+            }
+        }
+
+        wait(for: [promise], timeout: 4)
+    }
+
+    
 }
