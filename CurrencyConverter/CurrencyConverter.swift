9698e313932c4d0493b434948fd4a3ab81bf54c0
ms2138 <m38371@gmail.com>
2020-10-15 20:42:05 -0700

    Add CurrencyConverter class

---
+++ CurrencyConverter/CurrencyConverter.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyConverter.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-10-14.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+class CurrencyConverter {
+
+}
