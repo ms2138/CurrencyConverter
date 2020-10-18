0ad2a9230417b876bff7d4768e60b1f606a97eb2
ms2138 <m38371@gmail.com>
2020-10-17 20:37:54 -0700

    Add CurrencyConverterTests class

---
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -0,0 +1,20 @@
+//
+//  CurrencyConverterTests.swift
+//  CurrencyConverterTests
+//
+//  Created by mani on 2020-10-17.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import XCTest
+
+@testable import CurrencyConverter
+
+class CurrencyConverterTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+}
