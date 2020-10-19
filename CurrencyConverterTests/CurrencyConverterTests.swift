305218a0c95ccfff07f5fd62a407ff932159c8ba
ms2138 <m38371@gmail.com>
2020-10-18 19:50:46 -0700

    Add converter property and testConvert method to CurrencyConverterTests

--- CurrencyConverterTests/CurrencyConverterTests.swift
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -11,10 +11,18 @@ import XCTest
 @testable import CurrencyConverter
 
 class CurrencyConverterTests: XCTestCase {
+    var converter: CurrencyConverter!
 
     override func setUp() {
+        converter = CurrencyConverter(amount: 49.0)
     }
 
     override func tearDown() {
+    }
+
+    func testConvert() {
+        let total = converter.convert(rate: 1.3933)
+
+        XCTAssertEqual(total, 49.0 * 1.3933)
     }
 }
