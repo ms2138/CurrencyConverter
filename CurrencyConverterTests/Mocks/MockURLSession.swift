0dca02c072271ebcce60d6115845ef8cc53764b6
ms2138 <m38371@gmail.com>
2020-10-24 19:42:51 -0700

    Add data, response, and error properties to MockURLSession.  Add URLSessionProtocol conformance to MockURLSession.  Add init and dataTask method to MockURLSession

--- CurrencyConverterTests/Mocks/MockURLSession.swift
+++ CurrencyConverterTests/Mocks/MockURLSession.swift
@@ -10,6 +10,22 @@ import Foundation
 
 @testable import CurrencyConverter
 
-class MockURLSession {
+class MockURLSession: URLSessionProtocol {
+    let data: Data?
+    let response: URLResponse?
+    let error: Error?
 
+    init(data: Data?, response: URLResponse?, error: Error?) {
+        self.data = data
+        self.response = response
+        self.error = error
+    }
+
+    func dataTask(
+        with url: URL,
+        completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
+    ) -> URLSessionDataTask {
+        completionHandler(self.data, self.response, self.error)
+        return URLSession.shared.dataTask(with: url)
+    }
 }
