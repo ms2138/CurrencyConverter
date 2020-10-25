d89c17405f5cd1cde3502bc17a190313e68c30bd
ms2138 <m38371@gmail.com>
2020-10-24 19:24:22 -0700

    Add dataTask method to URLSessionProtocol.  Add URLSessionProtocol conformance to URLSession

--- CurrencyConverter/Testing/URLSessionProtocol.swift
+++ CurrencyConverter/Testing/URLSessionProtocol.swift
@@ -9,5 +9,11 @@
 import Foundation
 
 protocol URLSessionProtocol {
+    func dataTask(
+        with url: URL,
+        completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
+    ) -> URLSessionDataTask
 }
+
+extension URLSession: URLSessionProtocol {}
 
