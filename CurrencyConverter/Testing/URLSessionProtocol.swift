afcf0058983bc1df66649ff7f0ad6f140c60d844
ms2138 <m38371@gmail.com>
2020-10-24 19:18:25 -0700

    Add Testing folder.  Add URLSessionProtocol to Testing folder.  Add Mocks folder.  Add MockURLSession to Mocks folder

---
+++ CurrencyConverter/Testing/URLSessionProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  URLSessionProtocol.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-10-21.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+protocol URLSessionProtocol {
+}
+
---
+++ CurrencyConverterTests/Mocks/MockURLSession.swift
@@ -0,0 +1,15 @@
+//
+//  MockURLSession.swift
+//  CurrencyConverterTests
+//
+//  Created by mani on 2020-10-22.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+@testable import CurrencyConverter
+
+class MockURLSession {
+
+}
