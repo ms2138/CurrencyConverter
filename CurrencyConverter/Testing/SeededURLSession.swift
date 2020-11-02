efb9f5235aa14ba344e7a6ec8dd33fb49a43d91b
ms2138 <m38371@gmail.com>
2020-11-01 21:55:52 -0800

    Add URLSessionProtocol conformance to SeededURLSession.  Add implementation for dataTask method

--- CurrencyConverter/Testing/SeededURLSession.swift
+++ CurrencyConverter/Testing/SeededURLSession.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-class SeededURLSession {
+class SeededURLSession: URLSessionProtocol {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        if let json = ProcessInfo.processInfo.environment["ConversionSuccess"] {
+            let data = Data(json.utf8)
+            completionHandler(data, nil, nil)
+        }
+
+        if let errorDomain = ProcessInfo.processInfo.environment["ConversionFailure"] {
+            let error = NSError(domain: errorDomain, code: 0, userInfo: nil)
+            completionHandler(nil, nil, error)
+        }
+
+        return FakeDataTask()
+    }
 }
