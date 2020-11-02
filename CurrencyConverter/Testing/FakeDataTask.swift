258467659c122f68fa4d70c9f8d170ff4b657a30
ms2138 <m38371@gmail.com>
2020-11-01 21:34:27 -0800

    Add FakeDataTask class

---
+++ CurrencyConverter/Testing/FakeDataTask.swift
@@ -0,0 +1,12 @@
+//
+//  FakeDataTask.swift
+//  CurrencyConverter
+//
+//  Created by mani on 2020-11-01.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+class FakeDataTask {
+}
